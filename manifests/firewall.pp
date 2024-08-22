f133e44e7e18ed4e63c8331c065b106234ef51cd
Sam Morrison <sorrison@gmail.com>
2024-08-22 10:02:55 +1000

    Firewall action->jump
    Change-Id: I77cd6a26b840e1499f6ea1f77d5dc60b902dabff

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -6,10 +6,10 @@ class jenkins::firewall {
   if defined('::firewall') {
     firewall {
       '500 allow Jenkins inbound traffic':
-        action => 'accept',
-        state  => 'NEW',
-        dport  => 8080,
-        proto  => 'tcp',
+        jump  => 'ACCEPT',
+        state => 'NEW',
+        dport => 8080,
+        proto => 'tcp',
     }
   }
 }
